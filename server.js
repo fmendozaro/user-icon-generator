05f49927bdd761ac0d3bfcb2ca93f6f3caa05a6a
fer <fernandoza@gmail.com>
2018-09-25 11:27:00 -0500

    feat: return a random image from the icons folder

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var http = require('http');
+let http = require('http');
+let fs = require('fs');
+let genders = ['men', 'women'];
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    //Return the url part of the request object:
-    res.write(req.url);
-    res.end();
+    let rndNumber = Math.floor(Math.random() * 100);
+    let rndGender = genders[Math.floor(Math.random() * 2)];
+    let baseUrl = './icons/'+rndGender+'/'+rndNumber+'.jpg';
+    let img = fs.readFileSync(baseUrl);
+    res.writeHead(200, {'Content-Type': 'image/jpeg'});
+    res.end(img, 'binary');
 }).listen(8080);
+
+console.log('Server running at http://localhost:8080/');
